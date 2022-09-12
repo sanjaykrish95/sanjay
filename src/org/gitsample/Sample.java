967d788852fb7ac9ca32391788cad95f1425299a
sanjaykrish95 <sanjay31295@gmail.com>
2022-09-12 13:58:01 +0530

    practice

---
+++ src/org/gitsample/Sample.java
@@ -0,0 +1,21 @@
+package org.gitsample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		System.out.println("sanjay");
+
+		System.out.println("hari");
+
+		System.out.println("mohan");
+
+		System.out.println("vignesh");
+
+		System.out.println("guna");
+
+		System.out.println("qa");
+
+	}
+
+}
